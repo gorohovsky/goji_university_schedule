7490b95892da929ad29517c83e073009d7685ce5
Pavel Gorohovsky <pavel-gorohovsky@yandex.com>
2025-01-15 22:00:51 +0200

    Added missing association

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,6 +2,7 @@ class Section < ApplicationRecord
   belongs_to :teacher
   belongs_to :subject
   belongs_to :classroom
+  has_many   :enrollments, dependent: :destroy
   has_many   :section_schedules, dependent: :destroy
 
   accepts_nested_attributes_for :section_schedules
